63badf8325ad764ce525d5d9806356fabe2a2b64
Natraj Molala <natraj.molala@agilesphere.co.uk>
2017-08-26 10:32:50 +0100

    sort skip limit collections

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,12 +2,16 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading records', () => {
-  let joe;
+  let joe, maria, alex, zach;
 
   beforeEach((done) => {
+    alex = new User({ name: 'Alex' });
     joe = new User({ name: 'Joe' });
-		joe.save()
-			 .then(() => done());
+    maria = new User({ name: 'Maria' });
+    zach = new User({ name: 'Zach' });
+
+    Promise.all([ alex.save(), zach.save(), joe.save(), maria.save() ])
+      .then(() => done());
   });
 
 	it('find all users with name joe', (done) => {
@@ -23,7 +27,21 @@ describe('Reading records', () => {
         then((user) => {
           assert(user.name === joe.name);
           done();
-        })
+        });
   });
-  
+
+  it('can skip and limit the result set', (done) => {
+      User.find({})
+        .sort({ name: 1 })
+        .skip(1)
+        .limit(2)
+        .then((users) => {
+
+          assert(users.length === 2);
+          assert(users[0].name === 'Joe');
+          assert(users[1].name === 'Maria');
+          done();
+        });
+  });
+
 });
